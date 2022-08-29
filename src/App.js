d13ef275eecb936ebc181aec5f968ff136465065
Mert Ayda <48617781+mertayda@users.noreply.github.com>
2022-08-30 01:55:09 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import Analytics from './Components/Analytics';
+import Hero from './Components/Hero';
+import NavBar from './Components/NavBar';
+import Newletters from './Components/Newletters';
+import Cards from './Components/Cards';
+import Footer from './Components/Footer';
+
+function App() {
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Hero></Hero>
+      <Analytics></Analytics>
+      <Newletters></Newletters>
+      <Cards></Cards>
+      <Footer></Footer>
+    </div>
+
+  );
+}
+
+export default App;
